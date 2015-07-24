841d9b49a451b3cc353f7c432674f279c7f609ab
icetornado <ttran@infina.net>
2015-07-24 11:08:10 -0400

    Added documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## function makeCacheMatrix consists two pairs of getter/setter for 
+## 1. x - a variable to store a numeric matrix in a different enviroment
+## 2. inversedM - a local variable to store inversed matrix of x
 makeCacheMatrix <- function(x = matrix()) {
 	inversedM <- NULL
-    
-    set <- function(d, size) {
-        x <<- matrix(data = d, nrow = size, ncol = size)
-        print("x setter")
-        print(x)
+	 
+    set <- function(inMatrix) {
+        x <<- inMatrix
         inversedM <<- NULL
     }
     
@@ -29,13 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function takes a makeCacheMatrix "object" as an input argument
+## output is a inversed matrix of the matrix which is created by calling 
+## set method of the makeCacheMatrix "object"
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(magicMatrix, ...) {
     ## Return a matrix that is the inverse of 'x'
     invMatrix <- magicMatrix$getInversed()
-    print("inv M")
-    print(invMatrix)
     
     if(!is.null(invMatrix)) {
         message("getting cached inversed matrix")
@@ -43,11 +39,27 @@ cacheSolve <- function(x, ...) {
     }
     else {
         orgMatrix <- magicMatrix$get()
-        print("org M")
-        print(orgMatrix)
+        message("making a new inversed matrix")
         
         invMatrix <- solve(orgMatrix)
         magicMatrix$setInversed(invMatrix)
-        invMatrix
+        return(invMatrix)
     }
 }
+
+## usage:
+## 1/
+## myMatrix <- matrix(1:4, 2, 2)
+## myMagicMatrix <- makeCacheMatrix()
+## myMagicMatrix$set(myMatrix)
+## invMatrix <- cacheSolve(myMagicMatrix)
+## print(invMatrix)     ## expect get an inversed matrix
+
+## 2/
+## myMatrix <- matrix(5:8, 2, 2)
+## invMatrix <- cacheSolve(myMagicMatrix)
+## print(invMatrix)     ## expect to get from cache
+## myMagicMatrix$set(myMatrix)
+## invMatrix <- cacheSolve(myMagicMatrix)
+## print(invMatrix)     ## expect to get a new inversed matrix
+
